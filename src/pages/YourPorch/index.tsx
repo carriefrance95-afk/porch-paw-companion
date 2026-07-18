a9699f704be34f9721ad869d75e41b3705b89bb7
carriefrance95-afk <carriefrance95@gmail.com>
2026-07-18 02:18:00 +0000

    Fix SummaryCards import path

--- src/pages/YourPorch/index.tsx
+++ src/pages/YourPorch/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import WelcomeHero from './components/WelcomeHero';
-import SummaryCards from './components/SummaryCards';
+import SummaryCards from './SummaryCards';
 
 const YourPorch: React.FC = () => {
   const navigate = useNavigate();
